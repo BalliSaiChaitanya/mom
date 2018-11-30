47c7e0f939d38c14745ed38d8adae3eca7852a6e
mombotic <mombotic@gmail.com>
2018-11-30 14:11:42 +0530

    sdf

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
-$servername = "192.168.64.145";
-$username = "innovationcenter";
-$password = "innovation123$$";
-$dbname = "innovationcenter_mom";
+// $servername = "192.168.64.145";
+// $username = "innovationcenter";
+// $password = "innovation123$$";
+// $dbname = "innovationcenter_mom";
 
 // Create connection
-$con = new mysqli($servername, $username, $password, $dbname);
+// $con = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($con->connect_error) {
-    die("Connection failed: " . $con->connect_error);
-} 
+// if ($con->connect_error) {
+//     die("Connection failed: " . $con->connect_error);
+// } 
 	$method = $_SERVER['REQUEST_METHOD'];
 if($method=="POST"){
 	$requestBody=file_get_contents('php://input');
@@ -18,15 +18,15 @@ if($method=="POST"){
 	$flavor=$json->result->parameters->flavor;
 	$response=new \stdClass();
 	
-	$sql="select * from suggestions where cat = ".$flavour."";
-    $query=mysqli_query($con,$query);
-    $speec=" ";
-    while($row=mysqli_fetch_array($sql))
-    {
-        $speec =$speec." ".$row['name'];
-    }
-    $speech=$speec;
-    
+	// $sql="select * from suggestions where cat = ".$flavour."";
+ //    $query=mysqli_query($con,$query);
+ //    $speec=" ";
+ //    while($row=mysqli_fetch_array($sql))
+ //    {
+ //        $speec =$speec." ".$row['name'];
+ //    }
+ //    $speech=$speec;
+    $speech="shit shit shitty";
 
     $response->speech=$speech;
 	$response->displayText=$speech;	

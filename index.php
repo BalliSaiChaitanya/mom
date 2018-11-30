366b84c4963978b8120150900d518bb2017c6b0c
mombotic <mombotic@gmail.com>
2018-11-30 14:18:01 +0530

    sdf

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <?php
-// $servername = "192.168.64.145";
-// $username = "innovationcenter";
-// $password = "innovation123$$";
-// $dbname = "innovationcenter_mom";
+$servername = "192.168.64.145";
+$username = "innovationcenter";
+$password = "innovation123$$";
+$dbname = "innovationcenter_mom";
 
-// Create connection
-// $con = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-// if ($con->connect_error) {
-//     die("Connection failed: " . $con->connect_error);
-// } 
+//Create connection
+$con = new mysqli($servername, $username, $password, $dbname);
+//Check connection
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+} 
 	$method = $_SERVER['REQUEST_METHOD'];
-if($method=="POST"){
+//if($method=="POST"){
 	$requestBody=file_get_contents('php://input');
 	$json=json_decode($requestBody);
 	$text=$json->result->parameters->text;
 	$flavor=$json->result->parameters->flavor;
 	$response=new \stdClass();
 	
-	// $sql="select * from suggestions where cat = ".$flavour."";
+	$sql="select * from suggestions where cat = ".$flavour."";
  //    $query=mysqli_query($con,$query);
  //    $speec=" ";
  //    while($row=mysqli_fetch_array($sql))
@@ -26,16 +26,17 @@ if($method=="POST"){
  //        $speec =$speec." ".$row['name'];
  //    }
  //    $speech=$speec;
+	echo $sql;
     $speech="shit shit shitty";
 
     $response->speech=$speech;
 	$response->displayText=$speech;	
 	$response->source="webhook";
 	echo json_encode($response);
-}
-else{
-	echo "method not allowed";
-}
+// }
+// else{
+// 	echo "method not allowed";
+// }
 
     
 ?>

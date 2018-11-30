2866ea003744777e0b4874e92190537b2db1f502
mombotic <45478541+mombotic@users.noreply.github.com>
2018-11-30 14:02:47 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,39 +1,31 @@
 <?php
-
-$method = $_SERVER['REQUEST_METHOD'];
-
+	require_once('../dbconnect.php');
+	$method = $_SERVER['REQUEST_METHOD'];
 if($method=="POST"){
 	$requestBody=file_get_contents('php://input');
 	$json=json_decode($requestBody);
 	$text=$json->result->parameters->text;
 	$flavor=$json->result->parameters->flavor;
 	$response=new \stdClass();
-
-
-
-	$rec=json_decode(file_get_contents('http://innovationcenter.gitam.edu/test3/interaction/data.json'));
-
 	
+	$sql="select * from suggestions where cat = ".$flavour."";
+    $query=mysqli_query($con,$query);
+    $speec=" ";
+    while($row=mysqli_fetch_array($sql))
+    {
+        $speec =$speec." ".$row['name'];
+    }
+    $speech=$speec;
+    
 
-	switch ($flavor) {
-		case "fruits":
-			$speech="I would like you to try ".$rec->fruits;
-			break;
-		case "chocolate":
-			$speech="I would like you to try ".$rec->chocolate;
-			break;
-		default:
-			$speech="Try oreo";
-			break;
-	}
-
-
-	$response->speech=$speech;
+    $response->speech=$speech;
 	$response->displayText=$speech;	
 	$response->source="webhook";
 	echo json_encode($response);
-}else{
+}
+else{
 	echo "method not allowed";
 }
 
+    
 ?>

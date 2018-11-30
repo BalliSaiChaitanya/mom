093753fdde5c21bf8b20c5fa721d3f16214bc6bf
mombotic <45478541+mombotic@users.noreply.github.com>
2018-11-30 17:06:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if($method=="POST"){
 
 
 	$curlSession = curl_init();
-    curl_setopt($curlSession, CURLOPT_URL, 'https://chart.googleapis.com/chart?cht=p3&chs=250x100&chd=t:60,40&chl=Hello|World&chof=json');
+    curl_setopt($curlSession, CURLOPT_URL, 'http://innovationcenter.gitam.edu/test3/interaction/data.json');
     curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
     curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
 

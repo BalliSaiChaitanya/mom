76bb2bf82f4cd3044a7acdd596318976895cacf2
mombotic <mombotic@gmail.com>
2018-11-30 11:36:04 +0530

    2nd fuck

--- index.php
+++ index.php
@@ -15,10 +15,10 @@ if($method=="POST"){
 
 	switch ($flavor) {
 		case "fruits":
-			$speech="I would like you to try ".$rec->fruits;
+			$response->speech="I would like you to try ".$rec->fruits;
 			break;
 		case "chocolate":
-			$speech="I would like you to try ".$rec->chocolate;
+			$response->speech="I would like you to try ".$rec->chocolate;
 			break;
 		default:
 			$speech="Try oreo";
@@ -26,8 +26,7 @@ if($method=="POST"){
 	}
 
 
-	$response->speech="";
-	$response->displayText="";
+	
 	$response->source="webhook";
 	echo json_encode($response);
 }else{

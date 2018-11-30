02236595d89be86c7e7585763e5aad8ccfdc28e1
mombotic <mombotic@gmail.com>
2018-11-30 13:38:52 +0530

    sdfsdf

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ if($method=="POST"){
 	$response->speech=$speech;
 	$response->displayText=$speech;	
 	$response->source="webhook";
-	echo json_encode($rec);
+	echo json_encode($response);
 }else{
 	echo "method not allowed";
 }

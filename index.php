041946cdbfeb48d45c62e6f47a9a38e44268262f
mombotic <mombotic@gmail.com>
2018-11-30 12:08:52 +0530

    fuck3

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ $method = $_SERVER['REQUEST_METHOD'];
 if($method=="POST"){
 	$requestBody=file_get_contents('php://input');
 	$json=json_decode($requestBody);
-	$text=$json->queryResult->queryText;
-	$flavor=$json->queryResult->parameters->flavor;
+	$text=$json->result->parameters->text;
+	$flavor=$json->result->parameters->flavor;
 	$response=new \stdClass();
 
 

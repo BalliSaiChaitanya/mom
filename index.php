67d39daed6ada67cf1e35d2e02e2dfcb0cf3c74a
mombotic <mombotic@gmail.com>
2018-11-30 17:22:03 +0530

    all done hopefully

--- index.php
+++ index.php
@@ -1,37 +1,37 @@
 <?php
 
-// $method = $_SERVER['REQUEST_METHOD'];
+$method = $_SERVER['REQUEST_METHOD'];
 
-// if($method=="POST"){
-// 	$requestBody=file_get_contents('php://input');
-// 	$json=json_decode($requestBody);
-// 	$text=$json->result->parameters->text;
-// 	$flavor=$json->result->parameters->flavor;
-// 	$response=new \stdClass();
+if($method=="POST"){
+	$requestBody=file_get_contents('php://input');
+	$json=json_decode($requestBody);
+	$text=$json->result->parameters->text;
+	$flavor=$json->result->parameters->flavor;
+	$response=new \stdClass();
 
-	echo "Hello this is test yayayayaya";
-	echo file_get_contents('https://raw.githubusercontent.com/mombotic/mom/sub/data.json');
+	
+	$rec=json_decode(file_get_contents('https://raw.githubusercontent.com/mombotic/mom/sub/data.json'));
 	
 
-// 	switch ($flavor) {
-// 		case "fruits":
-// 			$speech="I would like you to try ".$rec->fruits;
-// 			break;
-// 		case "chocolate":
-// 			$speech="I would like you to try ".$rec->chocolate;
-// 			break;
-// 		default:
-// 			$speech="Try oreo";
-// 			break;
-// 	}
+	switch ($flavor) {
+		case "fruits":
+			$speech="I would like you to try ".$rec->fruits;
+			break;
+		case "chocolate":
+			$speech="I would like you to try ".$rec->chocolate;
+			break;
+		default:
+			$speech="Try oreo";
+			break;
+	}
 
 
-// 	$response->speech=$speech;
-// 	$response->displayText=$speech;	
-// 	$response->source="webhook";
-// 	echo json_encode($response);
-// }else{
-// 	echo "method not allowed";
-// }
+	$response->speech=$speech;
+	$response->displayText=$speech;	
+	$response->source="webhook";
+	echo json_encode($response);
+}else{
+	echo "method not allowed";
+}
 
 ?>
